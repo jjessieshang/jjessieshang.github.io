984cba046137a408c9ef76c45384e83dc2546804
jjessieshang <17jessies@gmail.com>
2023-06-05 18:54:32 -0700

    toggle dropdowns

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+
+//toggle experiences read more
+const plusButtons = document.querySelectorAll('.plus');
+
+plusButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const card = btn.closest('.card');
+    const text = card.querySelector('.read-more');
+    text.classList.toggle('read-more--open');
+  });
+});
+
+//link to projects
 function gotolink(link) {
     console.log(link.value);
     location.href = link.value;
